8a919d715f6aaadf91a4004c44545adff2c921d9
Tilda <ti3406ky-s@student.lu.se>
2021-02-26 14:31:51 +0100

    Update routing

--- edaf90-project/src/app/app-routing.module.ts
+++ edaf90-project/src/app/app-routing.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { SearchComponent } from './search/search.component';
 import { WantToReadComponent } from './want-to-read/want-to-read.component';
 import { ReadBooksComponent } from './read-books/read-books.component';
 import { AppComponent } from './app.component';
 
+const routes: Routes = [
+  {path: 'search',component: SearchComponent},
+  {path: 'want-to-read', component: WantToReadComponent},
+  {path: 'read-books', component: ReadBooksComponent}
+];   
+ 
+
 @NgModule({
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-    {path: 'search', component: SearchComponent},
-    {path: 'wantToRead', component: WantToReadComponent},
-    {path: 'readBooks', component: ReadBooksComponent}
-    ]),
-  ],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
+export const routingComponents = [SearchComponent, WantToReadComponent, ReadBooksComponent]
